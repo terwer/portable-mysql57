031a81d0ae003a53caf66e962ad52d2a3ecc66f3
terwer <youweics@163.com>
2022-05-25 11:01:57 +0800

    fix:fix script

--- init/init.sql
+++ init/init.sql
@@ -2,4 +2,4 @@ CREATE USER 'terwer'@'%' IDENTIFIED BY '123456';
 GRANT ALL PRIVILEGES ON *.* TO 'terwer'@'%' WITH GRANT OPTION;
 flush privileges;
 
--- create database kms16_release default character set utf8mb4 collate utf8mb4_general_ci
+-- create database mytest default character set utf8mb4 collate utf8mb4_general_ci
