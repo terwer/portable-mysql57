a4c3d123820722ac74e717785ee28749bcfbf944
zhangyue <yuezhangcs@gmail.com>
2023-08-14 23:40:17 +0800

    init

--- init/init.sql
+++ init/init.sql
@@ -3,3 +3,4 @@ GRANT ALL PRIVILEGES ON *.* TO 'terwer'@'%' WITH GRANT OPTION;
 flush privileges;
 
 -- create database mytest default character set utf8mb4 collate utf8mb4_general_ci
+-- create database mytest default character set utf8 collate utf8_general_ci
--- ip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "mysql ip info:"
-docker inspect portable-mysql57 | grep IPAddres
